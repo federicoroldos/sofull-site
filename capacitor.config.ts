1955c3cbd93259c6bbfb070263bb6353b4cc0727
federicoroldos <fede212yt@gmail.com>
2026-02-11 16:34:26 +0900

    fix displayed app's name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sofull.site',
-  appName: 'So Full!',
+  appName: '배불러! (So Full!)',
   webDir: 'dist',
   server: {
     cleartext: true
